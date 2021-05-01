f808269171fc70b65bf1cc8b2089cf8d89337364
courses-test <83268620+courses-test@users.noreply.github.com>
2021-04-30 21:00:09 -0600

    Remove comments

--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -4,9 +4,7 @@ import './bootstrap.min.css';
 class EmotionTable extends React.Component {
     render() {
       return (  
-        <div>
-          // {/*You can remove this line and the line below. */}
-          // {JSON.stringify(this.props.emotions)}
+        <div>          
           <table className="table table-bordered">
             <tbody>
             {
